6a55d75b1383f22314d8625b31a21a24a8c398b1
Zilu Zhao <zzl.zilu@gmail.com>
2023-04-07 23:14:54 +0200

    harvester

--- Scripts/role.harvester.js
+++ Scripts/role.harvester.js
@@ -1,14 +1,20 @@
 var roleHarvester = {
-
     /** @param {Creep} creep **/
     run: function(creep) {
-        if(creep.store.getFreeCapacity() > 0) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-            }
+        var CURRENT_JOB_HARVEST=1;
+        var CURRENT_JOB_DEPOSIT=0;
+        //set current job
+        if(creep.memory.currentJob==CURRENT_JOB_HARVEST && creep.getFreeCapacity() == 0){
+            creep.memory.currentJob=CURRENT_JOB_DEPOSIT;
         }
-        else {
+        //maybe change later
+        else if(!creep.memory.currentJob && creep.store[RESOURCE_ENERGY] == 0) {
+            creep.memory.currentJob=CURRENT_JOB_HARVEST;
+        }
+
+
+
+        if(creep.memory.currentJob==CURRENT_JOB_DEPOSIT) {
             var targets = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
                     return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_TOWER)  &&
@@ -21,7 +27,14 @@ var roleHarvester = {
                 }
             }
         }
+        else {
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+        }
     }
+
 };
 
 module.exports = roleHarvester;
