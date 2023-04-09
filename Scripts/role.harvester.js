9f12b45021bc33775e40322809066d1ad9cebad8
Zilu Zhao <zzl.zilu@gmail.com>
2023-04-09 03:45:03 +0200

    harvester closest

--- Scripts/role.harvester.js
+++ Scripts/role.harvester.js
@@ -22,8 +22,9 @@ var roleHarvester = {
                 }
             });
             if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                tar=creep.pos.findClosestByRange(targets);
+                if(creep.transfer(tar, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(tar, {visualizePathStyle: {stroke: '#ffffff'}});
                 }
             }
         }
---
+++ Scripts/role.room.js
@@ -0,0 +1,7 @@
+// calculate the required roles in the room
+
+var roleRoom= {
+
+};
+
+module.exports = roleRoom;
